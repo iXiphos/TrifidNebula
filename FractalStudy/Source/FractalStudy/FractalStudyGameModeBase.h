1eb9235677f90613bab52b6a94fd117770cdbb86
Aidan Murphy <44793359+iXiphos@users.noreply.github.com>
2022-01-30 22:26:24 -0500

    Adding start of SDFs

---
+++ FractalStudy/Source/FractalStudy/FractalStudyGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "FractalStudyGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class FRACTALSTUDY_API AFractalStudyGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
